0a747c382f495c22629a3d83ca58158a5fabe6bb
Julie-K-11 <juliekolesnyk@gmail.com>
2025-04-15 20:10:40 +0300

    Практика 3 - покращена

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -10,15 +10,16 @@
 
 #[test]
 fn envelope(){
-    const W: u32 = 25;
+    const W: u32 = 28;
     const H: u32 = 10;
 
     for y in 0..H {
         for x in 0..W{
+            let k:f32 = W as f32 / (H - 1) as f32;
             let is_hor = y == 0 || y == H - 1;
             let is_ver = x == 0 || x == W - 1;
-            let is_diag1 = x == y * W / (H - 1);
-            let is_diag2 = y * W / (H - 1) == W - x - 1;
+            let is_diag1 = x == (y as f32 * k) as u32;
+            let is_diag2 = (y as f32 * k) as u32 == W - x - 1;
             let to_show = is_hor || is_ver || is_diag1 || is_diag2;
 
             let sym= if to_show {"*"} else {" "};
