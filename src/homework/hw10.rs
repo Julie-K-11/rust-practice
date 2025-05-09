e70f39e830a13284db60024812a132327d01f657
Julie-K-11 <juliekolesnyk@gmail.com>
2025-05-09 10:25:55 +0300

    Практика №10

---
+++ src/homework/hw10.rs
@@ -0,0 +1,24 @@
+/*написати код:
+    - який перевіряє чи є число паліндромом (https://en.wikipedia.org/wiki/Palindromic_number)
+*/
+
+fn is_palindrome(x: u32) -> bool {
+    let s = x.to_string();
+    s.chars().eq(s.chars().rev())
+}
+
+#[test]
+fn test() {
+    let data =
+        [
+            (123, false),
+            (121, true),
+            (1221, true),
+        ];
+
+    data
+        .iter()
+        .for_each(|(n, exp)| {
+            assert_eq!(is_palindrome(*n), *exp);
+        });
+}
