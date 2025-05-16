d9b1cf6382136c82e92b5b61deb5976493c64486
Julie-K-11 <juliekolesnyk@gmail.com>
2025-05-16 11:19:22 +0300

    Практика оновлення рандому

--- src/homework/hw11.rs
+++ src/homework/hw11.rs
@@ -7,8 +7,8 @@
 use rand::Rng;
 
 fn gen_random_vector(n: usize) -> Vec<i32> {
-    let mut rng = rand::thread_rng();
-    (0..n).map(|_| rng.gen_range(10..=99)).collect()
+    let mut rng = rand::rng();
+    (0..n).map(|_| rng.random_range(10..=99)).collect()
 }
 
 fn min_adjacent_sum(data: &[i32]) -> (usize, i32) {
